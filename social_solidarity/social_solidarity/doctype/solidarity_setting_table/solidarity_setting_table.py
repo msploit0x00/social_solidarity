963a7f287cbbb1dd4bc10618fe402ea9dadc7632
msploit0x00 <minasploit1@outlook.com>
2024-07-17 14:48:10 +0300

    exporting new social solidarity docs

---
+++ social_solidarity/social_solidarity/doctype/solidarity_setting_table/solidarity_setting_table.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, ds and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class SolidaritySettingTable(Document):
+	pass
